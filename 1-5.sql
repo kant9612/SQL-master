01292ed206015149e630338b48e8cb712796cc01
kant9612 <kanstephentakayama@gmail.com>
2021-04-24 00:47:58 +0000

    1-5

---
+++ 1-5.sql
@@ -0,0 +1,11 @@
+-- 「FROM purchases」のあとにコードを追加し、priceカラムが「1000」のデータを取得してください
+
+SELECT *
+FROM purchases
+WHERE price = 1000;
+
+-- 「FROM purchases」のあとにコードを追加し、purchased_atカラムが「2017-07-01」のデータを取得してください
+
+SELECT *
+FROM purchases
+where purchased_at = "2017-07-01";
